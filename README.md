01e36446e4e3fe88aa367860ee4d200ee3779496
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-20 18:30:30 +0530

    product table and modifiy the Reame.md file

---
+++ README.md
@@ -0,0 +1,88 @@
+
+# E-commerce
+
+E-commerce (electronic commerce) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the internet. These business transactions occur, business-to-consumer (B2C), consumer-to-consumer or consumer-to-business.
+
+
+
+## Tech Stack
+
+**Client:** React, Redux, Material Ui, axios
+
+**Server:** Node, Express, Prisma, cors
+
+
+## Features
+
+- Responsive
+- Cross platform
+
+
+## 🚀 About Me
+An engineer or developer who works on both the front end (client-side) and the back end (server-side) of a website or application is called a full-stack developer. They may handle projects involving databases, APIs, or designing user-facing websites, as well as interacting with clients during development.
+
+
+## 🛠 Skills
+Javascript, HTML, CSS, React.js, Node.js
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/ruturajmengal2016/)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/ruturajmengal2016/project_1.git
+```
+
+Go to the project directory
+
+```bash
+  cd project_1
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+## Deployment
+
+To deploy this project run
+
+```bash
+  npm run build
+```
+
+
+## Lessons Learned
+
+What did you learn while building this project? What challenges did you face and how did you overcome them?
+
+
+## Roadmap
+
+- Additional browser support
+
+- Add more integrations
+
+
+![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
+
+
+## Screenshots
+
+![App Screenshot]("https://drive.google.com/file/d/1AiZUOp-X7MgTu_VGpzYwEPMdU6ADhNAM/view?usp=share_link")
+
--- src/Pages/OurStore.jsx
+++ src/Pages/OurStore.jsx
@@ -6,8 +6,37 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import DirectionsIcon from "@mui/icons-material/Directions";
 import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
 import { useParams } from "react-router-dom";
+import ProductTable from "../Components/Atoms/ProductsTable";
+import fetchData from "../axios/axios";
+
+function createData(name, calories, fat, carbs, protein) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+  };
+}
+
+const rows = [
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Donut", 452, 25.0, 51, 4.9),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Honeycomb", 408, 3.2, 87, 6.5),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Jelly Bean", 375, 0.0, 94, 0.0),
+  createData("KitKat", 518, 26.0, 65, 7.0),
+  createData("Lollipop", 392, 0.2, 98, 0.0),
+  createData("Marshmallow", 318, 0, 81, 2.0),
+  createData("Nougat", 360, 19.0, 9, 37.0),
+  createData("Oreo", 437, 18.0, 63, 4.0),
+];
 const OurStore = () => {
   const { title } = useParams();
+
   const chips = [
     {
       label: "Direction",
@@ -31,6 +60,13 @@ const OurStore = () => {
       },
     },
   ];
+
+  // React.useEffect(() => {
+  //   fetchData
+  //     .get("")
+  //     .then((res) => setRows(res))
+  //     .catch((error) => console.log(error));
+  // }, [title]);
   return (
     <Box className={Style.root} component="div">
       <Box component="div" className={Style.profile}>
@@ -65,22 +101,19 @@ const OurStore = () => {
           })}
         </Typography>
       </Box>
-      <Box
-        component="div"
-        sx={{ height: 200, backgroundColor: "primary.main" }}
-      ></Box>
-      <Box
-        component="div"
-        sx={{ height: 200, backgroundColor: "primary.main" }}
-      ></Box>
-      <Box
-        component="div"
-        sx={{ height: 200, backgroundColor: "primary.main" }}
-      ></Box>
-      <Box
-        component="div"
-        sx={{ height: 200, backgroundColor: "primary.main" }}
-      ></Box>
+      <Box component="div" sx={{ height: "fit-content" }}>
+        <iframe
+          title="location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238154.609070339!2d79.0881546!3d21.14580040000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0a5a31faf13%3A0x19b37d06d0bb3e2b!2sNagpur%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1684583229298!5m2!1sen!2sin"
+          width="100%"
+          height="500"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </Box>
+      <ProductTable rows={rows} />
     </Box>
   );
 };
